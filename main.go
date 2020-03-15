673251f58dd2e6f300a9ac8b08d5d6189b3f9635
lighttiger2505 <lighttiger2505@gmail.com>
2020-03-16 01:46:14 +0900

    Add trace log

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"flag"
+	"io"
 	"log"
 	"os"
 
@@ -13,46 +14,50 @@ import (
 
 var (
 	logfile string
+	trace   bool
 )
 
 func main() {
-	flag.StringVar(&logfile, "log", "", "logfile")
+	flag.StringVar(&logfile, "log", "", "also log to this file (in addition to stderr)")
+	flag.BoolVar(&trace, "trace", false, "print all requests and responses")
 	flag.Parse()
 
+	var logWriter io.Writer
 	if logfile != "" {
 		f, err := os.OpenFile(logfile, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0660)
 		if err != nil {
 			log.Fatal(err)
 		}
 		defer f.Close()
-		log.SetOutput(f)
+		logWriter = io.MultiWriter(os.Stderr, f)
+	} else {
+		logWriter = io.MultiWriter(os.Stderr)
 	}
+	log.SetOutput(logWriter)
 
 	if flag.NArg() != 0 {
 		flag.Usage()
 		os.Exit(1)
 	}
 
-	if logfile != "" {
-		f, err := os.OpenFile(logfile, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0660)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer f.Close()
-	}
-
 	// Initialize language server
-	log.Println("sqls: start service")
 	server := handler.NewServer()
 	handler := jsonrpc2.HandlerWithError(server.Handle)
 
+	// Set connect option
+	var connOpt []jsonrpc2.ConnOpt
+	if trace {
+		connOpt = append(connOpt, jsonrpc2.LogMessages(log.New(logWriter, "", 0)))
+	}
+
 	// Start language server
 	log.Println("sqls: reading on stdin, writing on stdout")
-	var connOpt []jsonrpc2.ConnOpt
 	<-jsonrpc2.NewConn(
 		context.Background(),
 		jsonrpc2.NewBufferedStream(stdrwc{}, jsonrpc2.VSCodeObjectCodec{}),
-		handler, connOpt...).DisconnectNotify()
+		handler,
+		connOpt...,
+	).DisconnectNotify()
 	log.Println("sqls: connections closed")
 }
 
