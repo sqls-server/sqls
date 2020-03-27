f319e5cc9239c9bffcb8a4e8a9c658d23d861c4f
lighttiger2505 <lighttiger2505@gmail.com>
2020-03-27 23:43:43 +0900

    Move completion handler and test

---
+++ internal/handler/completion.go
@@ -0,0 +1,32 @@
+package handler
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/lighttiger2505/sqls/internal/lsp"
+	"github.com/sourcegraph/jsonrpc2"
+)
+
+func (s *Server) handleTextDocumentCompletion(ctx context.Context, conn *jsonrpc2.Conn, req *jsonrpc2.Request) (result interface{}, err error) {
+	if req.Params == nil {
+		return nil, &jsonrpc2.Error{Code: jsonrpc2.CodeInvalidParams}
+	}
+
+	var params lsp.CompletionParams
+	if err := json.Unmarshal(*req.Params, &params); err != nil {
+		return nil, err
+	}
+
+	f, ok := s.files[params.TextDocument.URI]
+	if !ok {
+		return nil, fmt.Errorf("document not found: %s", params.TextDocument.URI)
+	}
+
+	completionItems, err := s.completer.Complete(f.Text, params)
+	if err != nil {
+		return nil, err
+	}
+	return completionItems, nil
+}
--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -205,28 +205,6 @@ func (s *Server) handleTextDocumentDidClose(ctx context.Context, conn *jsonrpc2.
 	return nil, nil
 }
 
-func (s *Server) handleTextDocumentCompletion(ctx context.Context, conn *jsonrpc2.Conn, req *jsonrpc2.Request) (result interface{}, err error) {
-	if req.Params == nil {
-		return nil, &jsonrpc2.Error{Code: jsonrpc2.CodeInvalidParams}
-	}
-
-	var params lsp.CompletionParams
-	if err := json.Unmarshal(*req.Params, &params); err != nil {
-		return nil, err
-	}
-
-	f, ok := s.files[params.TextDocument.URI]
-	if !ok {
-		return nil, fmt.Errorf("document not found: %s", params.TextDocument.URI)
-	}
-
-	completionItems, err := s.completer.Complete(f.Text, params)
-	if err != nil {
-		return nil, err
-	}
-	return completionItems, nil
-}
-
 func (s *Server) openFile(uri string, languageID string) error {
 	f := &File{
 		Text:       "",
