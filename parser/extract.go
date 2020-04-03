e150589a5376a27e1be29f4a281d71f9f42335a0
lighttiger2505 <lighttiger2505@gmail.com>
2020-04-03 21:51:10 +0900

    Add extract functions

---
+++ parser/extract.go
@@ -0,0 +1,94 @@
+package parser
+
+import (
+	"github.com/lighttiger2505/sqls/ast"
+	"github.com/lighttiger2505/sqls/ast/astutil"
+)
+
+func ExtractSelectExpr(parsed ast.TokenList) []ast.Node {
+	prefixMatcher := astutil.NodeMatcher{
+		ExpectKeyword: []string{
+			"SELECT",
+			"ALL",
+			"DISTINCT",
+		},
+	}
+	peekMatcher := astutil.NodeMatcher{
+		NodeTypes: []ast.NodeType{
+			ast.TypeIdentiferList,
+			ast.TypeIdentifer,
+			ast.TypeMemberIdentifer,
+			ast.TypeOperator,
+			ast.TypeAliased,
+			ast.TypeParenthesis,
+			ast.TypeFunctionLiteral,
+		},
+	}
+	return filterPrefixGroup(astutil.NewNodeReader(parsed), prefixMatcher, peekMatcher)
+}
+
+func ExtractTableReferences(parsed ast.TokenList) []ast.Node {
+	prefixMatcher := astutil.NodeMatcher{
+		ExpectKeyword: []string{
+			"FROM",
+			"UPDATE",
+		},
+	}
+	peekMatcher := astutil.NodeMatcher{
+		NodeTypes: []ast.NodeType{
+			ast.TypeIdentiferList,
+			ast.TypeIdentifer,
+			ast.TypeMemberIdentifer,
+			ast.TypeAliased,
+		},
+	}
+	return filterPrefixGroup(astutil.NewNodeReader(parsed), prefixMatcher, peekMatcher)
+}
+
+func ExtractTableReference(parsed ast.TokenList) []ast.Node {
+	prefixMatcher := astutil.NodeMatcher{
+		ExpectKeyword: []string{
+			"INSERT INTO",
+			"UPDATE",
+			"DELETE FROM",
+		},
+	}
+	peekMatcher := astutil.NodeMatcher{
+		NodeTypes: []ast.NodeType{
+			ast.TypeIdentifer,
+			ast.TypeMemberIdentifer,
+			ast.TypeAliased,
+		},
+	}
+	return filterPrefixGroup(astutil.NewNodeReader(parsed), prefixMatcher, peekMatcher)
+}
+
+func ExtractWhereCondition(parsed ast.TokenList) []ast.Node {
+	prefixMatcher := astutil.NodeMatcher{
+		ExpectKeyword: []string{
+			"WHERE",
+		},
+	}
+	peekMatcher := astutil.NodeMatcher{
+		NodeTypes: []ast.NodeType{
+			ast.TypeComparison,
+			ast.TypeIdentiferList,
+		},
+	}
+	return filterPrefixGroup(astutil.NewNodeReader(parsed), prefixMatcher, peekMatcher)
+}
+
+func filterPrefixGroup(reader *astutil.NodeReader, prefixMatcher astutil.NodeMatcher, peekMatcher astutil.NodeMatcher) []ast.Node {
+	var results []ast.Node
+	for reader.NextNode(false) {
+		if reader.CurNodeIs(prefixMatcher) && reader.PeekNodeIs(true, peekMatcher) {
+			_, node := reader.PeekNode(true)
+			results = append(results, node)
+		}
+		if list, ok := reader.CurNode.(ast.TokenList); ok {
+			newReader := astutil.NewNodeReader(list)
+			results = append(results, filterPrefixGroup(newReader, prefixMatcher, peekMatcher)...)
+		}
+	}
+	return results
+}
---
+++ parser/extract_test.go
@@ -0,0 +1,165 @@
+package parser
+
+import (
+	"testing"
+)
+
+func TestExtractSelectExpr(t *testing.T) {
+	testcases := []struct {
+		name  string
+		input string
+		want  string
+	}{
+		{
+			name:  "",
+			input: "SELECT abc",
+			want:  "abc",
+		},
+		{
+			name:  "",
+			input: "SELECT abc, def",
+			want:  "abc, def",
+		},
+		{
+			name:  "",
+			input: "SELECT ALL abc",
+			want:  "abc",
+		},
+		{
+			name:  "",
+			input: "SELECT DISTINCT abc",
+			want:  "abc",
+		},
+	}
+	for _, tt := range testcases {
+		t.Run(tt.name, func(t *testing.T) {
+			query := initExtractTable(t, tt.input)
+			got := ExtractSelectExpr(query)
+			if len(got) == 0 {
+				t.Fatalf("not found filterd node")
+			}
+			if tt.want != got[0].String() {
+				t.Errorf("expected %q, got %q", tt.want, got[0].String())
+			}
+		})
+	}
+}
+
+func TestExtractTableReferences(t *testing.T) {
+	testcases := []struct {
+		name  string
+		input string
+		want  string
+	}{
+		{
+			name:  "",
+			input: "FROM abc",
+			want:  "abc",
+		},
+		{
+			name:  "",
+			input: "FROM abc, def",
+			want:  "abc, def",
+		},
+		{
+			name:  "",
+			input: "UPDATE abc",
+			want:  "abc",
+		},
+		{
+			name:  "",
+			input: "UPDATE abc, def",
+			want:  "abc, def",
+		},
+	}
+	for _, tt := range testcases {
+		t.Run(tt.name, func(t *testing.T) {
+			query := initExtractTable(t, tt.input)
+			got := ExtractTableReferences(query)
+			if len(got) == 0 {
+				t.Fatalf("not found filterd node")
+			}
+			if tt.want != got[0].String() {
+				t.Errorf("expected %q, got %q", tt.want, got[0].String())
+			}
+		})
+	}
+}
+
+func TestExtractTableReference(t *testing.T) {
+	testcases := []struct {
+		name  string
+		input string
+		want  string
+	}{
+		{
+			name:  "INSERT INTO",
+			input: "INSERT INTO abc",
+			want:  "abc",
+		},
+		{
+			name:  "UPDATE",
+			input: "UPDATE abc",
+			want:  "abc",
+		},
+		// FIXME
+		// {
+		// 	name:  "DELETE FROM",
+		// 	input: "DELETE FROM abc",
+		// 	want:  "abc",
+		// },
+	}
+	for _, tt := range testcases {
+		t.Run(tt.name, func(t *testing.T) {
+			query := initExtractTable(t, tt.input)
+			got := ExtractTableReference(query)
+			if len(got) == 0 {
+				t.Fatalf("not found filterd node")
+			}
+			if tt.want != got[0].String() {
+				t.Errorf("expected %q, got %q", tt.want, got[0].String())
+			}
+		})
+	}
+}
+
+func TestExtractWhereConditon(t *testing.T) {
+	testcases := []struct {
+		name  string
+		input string
+		want  string
+	}{
+		{
+			name:  "only",
+			input: "WHERE id = 123",
+			want:  "id = 123",
+		},
+		{
+			name:  "SELECT",
+			input: "SELECT id, name FROM abc WHERE id = 123",
+			want:  "id = 123",
+		},
+		{
+			name:  "UPDATE",
+			input: "UPDATE abc SET name = 'foo' WHERE id = 123",
+			want:  "id = 123",
+		},
+		{
+			name:  "DELETE",
+			input: "DELETE FROM abc WHERE id = 123",
+			want:  "id = 123",
+		},
+	}
+	for _, tt := range testcases {
+		t.Run(tt.name, func(t *testing.T) {
+			query := initExtractTable(t, tt.input)
+			got := ExtractWhereCondition(query)
+			if len(got) == 0 {
+				t.Fatalf("not found filterd node")
+			}
+			if tt.want != got[0].String() {
+				t.Errorf("expected %q, got %q", tt.want, got[0].String())
+			}
+		})
+	}
+}
