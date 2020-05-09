8599a125ff153a614bf6fc3e1f2f187f745e9ebe
lighttiger2505 <lighttiger2505@gmail.com>
2020-05-10 02:36:13 +0900

    Clean config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -12,7 +11,6 @@ import (
 )
 
 var ymlConfigPath = configFilePath("config.yml")
-var jsonConfigPath = configFilePath("config.json")
 
 type Config struct {
 	Connections []*database.Config `json:"connections" yaml:"connections"`
@@ -25,54 +23,22 @@ func newConfig() *Config {
 
 func GetConfig() (*Config, error) {
 	cfg := newConfig()
-	if err := cfg.Load(); err != nil {
+	if err := cfg.Load(ymlConfigPath); err != nil {
 		return nil, err
 	}
 	return cfg, nil
 }
 
-func (c *Config) Path() string {
-	return ymlConfigPath
+func GetConfigWithPath(fp string) (*Config, error) {
+	cfg := newConfig()
+	if err := cfg.Load(fp); err != nil {
+		return nil, err
+	}
+	return cfg, nil
 }
 
-func (c *Config) Read() (string, error) {
-	if err := os.MkdirAll(filepath.Dir(ymlConfigPath), 0700); err != nil {
-		return "", xerrors.Errorf("cannot create directory, %+v", err)
-	}
-
-	if !IsFileExist(ymlConfigPath) {
-		_, err := os.Create(ymlConfigPath)
-		if err != nil {
-			return "", xerrors.Errorf("cannot create config, %+v", err.Error())
-		}
-	}
-
-	file, err := os.OpenFile(ymlConfigPath, os.O_RDONLY, 0666)
-	if err != nil {
-		return "", xerrors.Errorf("cannot open config, %+v", err)
-	}
-	defer file.Close()
-
-	b, err := ioutil.ReadAll(file)
-	if err != nil {
-		return "", xerrors.Errorf("cannot read config, %+v", err)
-	}
-
-	return string(b), nil
-}
-
-func (c *Config) Load() error {
-	if err := os.MkdirAll(filepath.Dir(ymlConfigPath), 0700); err != nil {
-		return xerrors.Errorf("cannot create directory, %+v", err)
-	}
-
-	if !IsFileExist(ymlConfigPath) {
-		if err := createNewConfig(); err != nil {
-			return err
-		}
-	}
-
-	file, err := os.OpenFile(ymlConfigPath, os.O_RDONLY, 0666)
+func (c *Config) Load(fp string) error {
+	file, err := os.OpenFile(fp, os.O_RDONLY, 0666)
 	if err != nil {
 		return xerrors.Errorf("cannot open config, %+v", err)
 	}
@@ -85,56 +51,6 @@ func (c *Config) Load() error {
 
 	if err = yaml.Unmarshal(b, c); err != nil {
 		return xerrors.Errorf("failed unmarshal yaml, %+v", err, string(b))
-	}
-	return nil
-}
-
-func (c *Config) Save() error {
-	file, err := os.OpenFile(ymlConfigPath, os.O_WRONLY, 0666)
-	if err != nil {
-		return xerrors.Errorf("cannot open file, %+v", err)
-	}
-	defer file.Close()
-
-	out, err := yaml.Marshal(c)
-	if err != nil {
-		return xerrors.Errorf("cannot marshal config, %+v", err)
-	}
-
-	if _, err = io.WriteString(file, string(out)); err != nil {
-		return xerrors.Errorf("cannot write config file, %+v", err)
-	}
-	return nil
-}
-
-func createNewConfig() error {
-	// Create new config file
-	_, err := os.Create(ymlConfigPath)
-	if err != nil {
-		return xerrors.Errorf("cannot create config, %+v", err)
-	}
-
-	// Add default settings
-	cfg := newConfig()
-	cfg.Connections = []*database.Config{
-		{
-			Driver:         "mysql",
-			DataSourceName: "",
-			Proto:          "tcp",
-			User:           "root",
-			Passwd:         "root",
-			Host:           "127.0.0.1",
-			Port:           13306,
-			Path:           "",
-			DBName:         "world",
-			Params: map[string]string{
-				"tls":        "skip-verify",
-				"autocommit": "true",
-			},
-		},
-	}
-	if err := cfg.Save(); err != nil {
-		return err
 	}
 	return nil
 }
