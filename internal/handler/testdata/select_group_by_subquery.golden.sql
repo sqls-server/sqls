ca4018b999d1d674bfb6d2aab9267cc050daaab4
lighttiger2505 <lighttiger2505@gmail.com>
2020-10-02 00:34:01 +0900

    Fix test

--- internal/handler/testdata/select_group_by_subquery.golden.sql
+++ internal/handler/testdata/select_group_by_subquery.golden.sql
@@ -1,15 +1,17 @@
-select 
+select
 	*,
 	sum_b + 2 as mod_sum
-from (
-	select
-		a,
-		sum(b) as sum_b
-	from
-		table
-	group by
-		a, z
-)
+from
+	(
+		select
+			a,
+			sum(b) as sum_b
+		from
+			table
+		group by
+			a,
+			z
+	)
 order by
 	1,
 	2
