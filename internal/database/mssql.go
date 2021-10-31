sqls-server/sqls

commit d2cda9c90e1574db4702658e248f2dabef1050d1

Added MSSQL support
by using driver `github.com/denisenkom/go-mssqldb`.

Note that only SQL authentication is support (that is, Windows
authentication is not supported yet.)

internal/database/mssql.go
@@ -1,0 +1,342 @@
+package database
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/url"
+	"strconv"
+
+	_ "github.com/denisenkom/go-mssqldb"
+	"github.com/lighttiger2505/sqls/dialect"
+	"golang.org/x/crypto/ssh"
+)
+
+func init() {
+	RegisterOpen("mssql", mssqlOpen)
+	RegisterFactory("mssql", NewMssqlDBRepository)
+}
+
+func mssqlOpen(dbConnCfg *DBConfig) (*DBConnection, error) {
+	var (
+		conn    *sql.DB
+		sshConn *ssh.Client
+	)
+	dsn, err := genMssqlConfig(dbConnCfg)
+	if err != nil {
+		return nil, err
+	}
+
+	if dbConnCfg.SSHCfg != nil {
+		return nil, fmt.Errorf("connect via SSH is not supported")
+	}
+	dbConn, err := sql.Open("sqlserver", dsn)
+	if err != nil {
+		return nil, err
+	}
+	conn = dbConn
+	if err = conn.Ping(); err != nil {
+		return nil, err
+	}
+
+	conn.SetMaxIdleConns(DefaultMaxIdleConns)
+	conn.SetMaxOpenConns(DefaultMaxOpenConns)
+
+	return &DBConnection{
+		Conn:    conn,
+		SSHConn: sshConn,
+	}, nil
+}
+
+type MssqlDBRepository struct {
+	Conn *sql.DB
+}
+
+func NewMssqlDBRepository(conn *sql.DB) DBRepository {
+	return &MssqlDBRepository{Conn: conn}
+}
+
+func (db *MssqlDBRepository) Driver() dialect.DatabaseDriver {
+	return dialect.DatabaseDriverMssql
+}
+
+func (db *MssqlDBRepository) CurrentDatabase(ctx context.Context) (string, error) {
+	row := db.Conn.QueryRowContext(ctx, "SELECT DB_NAME()")
+	var database string
+	if err := row.Scan(&database); err != nil {
+		return "", err
+	}
+	return database, nil
+}
+
+func (db *MssqlDBRepository) Databases(ctx context.Context) ([]string, error) {
+	rows, err := db.Conn.QueryContext(
+		ctx,
+		`
+	SELECT name FROM sys.databases
+	`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	databases := []string{}
+	for rows.Next() {
+		var database string
+		if err := rows.Scan(&database); err != nil {
+			return nil, err
+		}
+		databases = append(databases, database)
+	}
+	return databases, nil
+}
+
+func (db *MssqlDBRepository) CurrentSchema(ctx context.Context) (string, error) {
+	row := db.Conn.QueryRowContext(ctx, "SELECT SCHEMA_NAME()")
+	var database sql.NullString
+	if err := row.Scan(&database); err != nil {
+		return "", err
+	}
+	if database.Valid {
+		return database.String, nil
+	}
+	return "", nil
+}
+
+func (db *MssqlDBRepository) Schemas(ctx context.Context) ([]string, error) {
+	rows, err := db.Conn.QueryContext(
+		ctx,
+		`
+	SELECT schema_name FROM information_schema.schemata
+	`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	databases := []string{}
+	for rows.Next() {
+		var database string
+		if err := rows.Scan(&database); err != nil {
+			return nil, err
+		}
+		databases = append(databases, database)
+	}
+	return databases, nil
+}
+
+func (db *MssqlDBRepository) SchemaTables(ctx context.Context) (map[string][]string, error) {
+	rows, err := db.Conn.QueryContext(
+		ctx,
+		`
+	SELECT
+		table_schema,
+		table_name
+	FROM
+		information_schema.tables
+	ORDER BY
+		table_schema,
+		table_name
+	`)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	databaseTables := map[string][]string{}
+	for rows.Next() {
+		var schema, table string
+		if err := rows.Scan(&schema, &table); err != nil {
+			return nil, err
+		}
+
+		if arr, ok := databaseTables[schema]; ok {
+			databaseTables[schema] = append(arr, table)
+		} else {
+			databaseTables[schema] = []string{table}
+		}
+	}
+	return databaseTables, nil
+}
+
+func (db *MssqlDBRepository) Tables(ctx context.Context) ([]string, error) {
+	rows, err := db.Conn.QueryContext(
+		ctx,
+		`
+	SELECT
+	  table_name
+	FROM
+	  information_schema.tables
+	WHERE
+	  table_type = 'BASE TABLE'
+	  AND table_schema NOT IN ('information_schema')
+	ORDER BY
+	  table_name
+	`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	tables := []string{}
+	for rows.Next() {
+		var table string
+		if err := rows.Scan(&table); err != nil {
+			return nil, err
+		}
+		tables = append(tables, table)
+	}
+	return tables, nil
+}
+
+func (db *MssqlDBRepository) DescribeDatabaseTable(ctx context.Context) ([]*ColumnDesc, error) {
+	rows, err := db.Conn.QueryContext(
+		ctx,
+		`
+	SELECT
+		c.table_schema,
+		c.table_name,
+		c.column_name,
+		c.data_type,
+		c.is_nullable,
+		CASE tc.constraint_type
+			WHEN 'PRIMARY KEY' THEN 'YES'
+			ELSE 'NO'
+		END,
+		c.column_default,
+		''
+	FROM
+		information_schema.columns c
+	LEFT JOIN
+		information_schema.constraint_column_usage ccu
+		ON c.table_name = ccu.table_name
+		AND c.column_name = ccu.column_name
+	LEFT JOIN information_schema.table_constraints tc ON
+		tc.table_catalog = c.table_catalog
+		AND tc.table_schema = c.table_schema
+		AND tc.table_name = c.table_name
+		AND tc.constraint_name = ccu.constraint_name
+	ORDER BY
+		c.table_name,
+		c.ordinal_position
+	`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	tableInfos := []*ColumnDesc{}
+	for rows.Next() {
+		var tableInfo ColumnDesc
+		err := rows.Scan(
+			&tableInfo.Schema,
+			&tableInfo.Table,
+			&tableInfo.Name,
+			&tableInfo.Type,
+			&tableInfo.Null,
+			&tableInfo.Key,
+			&tableInfo.Default,
+			&tableInfo.Extra,
+		)
+		if err != nil {
+			return nil, err
+		}
+		tableInfos = append(tableInfos, &tableInfo)
+	}
+	return tableInfos, nil
+}
+
+func (db *MssqlDBRepository) DescribeDatabaseTableBySchema(ctx context.Context, schemaName string) ([]*ColumnDesc, error) {
+	rows, err := db.Conn.QueryContext(
+		ctx,
+		`
+	SELECT
+		c.table_schema,
+		c.table_name,
+		c.column_name,
+		c.data_type,
+		c.is_nullable,
+		CASE tc.constraint_type
+			WHEN 'PRIMARY KEY' THEN 'YES'
+			ELSE 'NO'
+		END,
+		c.column_default,
+		''
+	FROM
+		information_schema.columns c
+	LEFT JOIN
+		information_schema.constraint_column_usage ccu
+		ON c.table_name = ccu.table_name
+		AND c.column_name = ccu.column_name
+	LEFT JOIN information_schema.table_constraints tc ON
+		tc.table_catalog = c.table_catalog
+		AND tc.table_schema = c.table_schema
+		AND tc.table_name = c.table_name
+		AND tc.constraint_name = ccu.constraint_name
+	WHERE
+		c.table_schema = $1
+	ORDER BY
+		c.table_name,
+		c.ordinal_position
+	`, schemaName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	tableInfos := []*ColumnDesc{}
+	for rows.Next() {
+		var tableInfo ColumnDesc
+		err := rows.Scan(
+			&tableInfo.Schema,
+			&tableInfo.Table,
+			&tableInfo.Name,
+			&tableInfo.Type,
+			&tableInfo.Null,
+			&tableInfo.Key,
+			&tableInfo.Default,
+			&tableInfo.Extra,
+		)
+		if err != nil {
+			return nil, err
+		}
+		tableInfos = append(tableInfos, &tableInfo)
+	}
+	return tableInfos, nil
+}
+
+func (db *MssqlDBRepository) Exec(ctx context.Context, query string) (sql.Result, error) {
+	return db.Conn.ExecContext(ctx, query)
+}
+
+func (db *MssqlDBRepository) Query(ctx context.Context, query string) (*sql.Rows, error) {
+	return db.Conn.QueryContext(ctx, query)
+}
+
+func genMssqlConfig(connCfg *DBConfig) (string, error) {
+	if connCfg.DataSourceName != "" {
+		return connCfg.DataSourceName, nil
+	}
+
+	q := url.Values{}
+	q.Set("user", connCfg.User)
+	q.Set("password", connCfg.Passwd)
+	q.Set("database", connCfg.DBName)
+
+	switch connCfg.Proto {
+	case ProtoTCP:
+		host, port := connCfg.Host, connCfg.Port
+		if host == "" {
+			host = "127.0.0.1"
+		}
+		if port == 0 {
+			port = 1433
+		}
+		q.Set("host", host)
+		q.Set("port", strconv.Itoa(port))
+	case ProtoUDP, ProtoUnix:
+	default:
+		return "", fmt.Errorf("default addr for network %s unknown", connCfg.Proto)
+	}
+
+	for k, v := range connCfg.Params {
+		q.Set(k, v)
+	}
+
+	return genOptions(q, "", "=", ";", ",", true), nil
+}

internal/database/mssql_test.go
@@ -1,0 +1,49 @@
+package database
+
+import (
+	"testing"
+
+	_ "github.com/denisenkom/go-mssqldb"
+)
+
+func Test_genMssqlConfig(t *testing.T) {
+	tests := []struct {
+		name    string
+		connCfg *DBConfig
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "",
+			connCfg: &DBConfig{
+				Alias:          "",
+				Driver:         "mssql",
+				DataSourceName: "",
+				Proto:          "tcp",
+				User:           "sa",
+				Passwd:         "mysecretpassword1234",
+				Host:           "127.0.0.1",
+				Port:           11433,
+				Path:           "",
+				DBName:         "dvdrental",
+				Params: map[string]string{
+					"ApplicationIntent": "ReadOnly",
+				},
+			},
+			want:    "ApplicationIntent=ReadOnly;database=dvdrental;host=127.0.0.1;password=mysecretpassword1234;port=11433;user=sa",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := genMssqlConfig(tt.connCfg)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("genMssqlConfig() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("got %q, want %q", got, tt.want)
+			}
+		})
+	}
+}

dialect/mssql.go
@@ -1,0 +1,190 @@
+package dialect
+
+// https://docs.microsoft.com/en-us/sql/t-sql/language-elements/reserved-keywords-transact-sql
+var mssqlKeywords = []string{
+	"ADD",
+	"ALL",
+	"ALTER",
+	"AND",
+	"ANY",
+	"AS",
+	"ASC",
+	"AUTHORIZATION",
+	"BACKUP",
+	"BEGIN",
+	"BETWEEN",
+	"BREAK",
+	"BROWSE",
+	"BULK",
+	"BY",
+	"CASCADE",
+	"CASE",
+	"CHECK",
+	"CHECKPOINT",
+	"CLOSE",
+	"CLUSTERED",
+	"COALESCE",
+	"COLLATE",
+	"COLUMN",
+	"COMMIT",
+	"COMPUTE",
+	"CONSTRAINT",
+	"CONTAINS",
+	"CONTAINSTABLE",
+	"CONTINUE",
+	"CONVERT",
+	"CREATE",
+	"CROSS",
+	"CURRENT",
+	"CURRENT_DATE",
+	"CURRENT_TIME",
+	"CURRENT_TIMESTAMP",
+	"CURRENT_USER",
+	"CURSOR",
+	"DATABASE",
+	"DBCC",
+	"DEALLOCATE",
+	"DECLARE",
+	"DEFAULT",
+	"DELETE",
+	"DENY",
+	"DESC",
+	"DISK",
+	"DISTINCT",
+	"DISTRIBUTED",
+	"DOUBLE",
+	"DROP",
+	"DUMP",
+	"ELSE",
+	"END",
+	"ERRLVL",
+	"ESCAPE",
+	"EXCEPT",
+	"EXEC",
+	"EXECUTE",
+	"EXISTS",
+	"EXIT",
+	"EXTERNAL",
+	"FETCH",
+	"FILE",
+	"FILLFACTOR",
+	"FOR",
+	"FOREIGN",
+	"FREETEXT",
+	"FREETEXTTABLE",
+	"FROM",
+	"FULL",
+	"FUNCTION",
+	"GOTO",
+	"GRANT",
+	"GROUP",
+	"HAVING",
+	"HOLDLOCK",
+	"IDENTITY",
+	"IDENTITYCOL",
+	"IDENTITY_INSERT",
+	"IF",
+	"IN",
+	"INDEX",
+	"INNER",
+	"INSERT",
+	"INTERSECT",
+	"INTO",
+	"IS",
+	"JOIN",
+	"KEY",
+	"KILL",
+	"LEFT",
+	"LIKE",
+	"LINENO",
+	"LOAD",
+	"MERGE",
+	"NATIONAL",
+	"NOCHECK",
+	"NONCLUSTERED",
+	"NOT",
+	"NULL",
+	"NULLIF",
+	"OF",
+	"OFF",
+	"OFFSETS",
+	"ON",
+	"OPEN",
+	"OPENDATASOURCE",
+	"OPENQUERY",
+	"OPENROWSET",
+	"OPENXML",
+	"OPTION",
+	"OR",
+	"ORDER",
+	"OUTER",
+	"OVER",
+	"PERCENT",
+	"PIVOT",
+	"PLAN",
+	"PRECISION",
+	"PRIMARY",
+	"PRINT",
+	"PROC",
+	"PROCEDURE",
+	"PUBLIC",
+	"RAISERROR",
+	"READ",
+	"READTEXT",
+	"RECONFIGURE",
+	"REFERENCES",
+	"REPLICATION",
+	"RESTORE",
+	"RESTRICT",
+	"RETURN",
+	"REVERT",
+	"REVOKE",
+	"RIGHT",
+	"ROLLBACK",
+	"ROWCOUNT",
+	"ROWGUIDCOL",
+	"RULE",
+	"SAVE",
+	"SCHEMA",
+	"SECURITYAUDIT",
+	"SELECT",
+	"SEMANTICKEYPHRASETABLE",
+	"SEMANTICSIMILARITYDETAILSTABLE",
+	"SEMANTICSIMILARITYTABLE",
+	"SESSION_USER",
+	"SET",
+	"SETUSER",
+	"SHUTDOWN",
+	"SOME",
+	"STATISTICS",
+	"SYSTEM_USER",
+	"TABLE",
+	"TABLESAMPLE",
+	"TEXTSIZE",
+	"THEN",
+	"TO",
+	"TOP",
+	"TRAN",
+	"TRANSACTION",
+	"TRIGGER",
+	"TRUNCATE",
+	"TRY_CONVERT",
+	"TSEQUAL",
+	"UNION",
+	"UNIQUE",
+	"UNPIVOT",
+	"UPDATE",
+	"UPDATETEXT",
+	"USE",
+	"USER",
+	"VALUES",
+	"VARYING",
+	"VIEW",
+	"WAITFOR",
+	"WHEN",
+	"WHERE",
+	"WHILE",
+	"WITH",
+	"WITHIN GROUP",
+	"WRITETEXT",
+}

dialect/keyword.go
@@ -385,6 +385,7 @@ const (
 	DatabaseDriverMySQL56    DatabaseDriver = "mysql56"
 	DatabaseDriverPostgreSQL DatabaseDriver = "postgresql"
 	DatabaseDriverSQLite3    DatabaseDriver = "sqlite3"
+	DatabaseDriverMssql      DatabaseDriver = "mssql"
 )
 
 func DataBaseKeywords(driver DatabaseDriver) []string {
@@ -401,6 +402,8 @@ func DataBaseKeywords(driver DatabaseDriver) []string {
 		return postgresql13Keywords
 	case DatabaseDriverSQLite3:
 		return sqliteKeywords
+	case DatabaseDriverMssql:
+		return mssqlKeywords
 	default:
 		return sqliteKeywords
 	}
@@ -419,6 +422,8 @@ func DataBaseFunctions(driver DatabaseDriver) []string {
 	case DatabaseDriverPostgreSQL:
 		return []string{}
 	case DatabaseDriverSQLite3:
+		return []string{}
+	case DatabaseDriverMssql:
 		return []string{}
 	default:
 		return []string{}

docker-compose.yml
@@ -45,7 +45,7 @@ services:
   postgres12:
     image: postgres:12-alpine
     container_name: sqls_postgres12
-    ports: 
+    ports:
       - "15432:5432"
     environment:
       - POSTGRES_USER=postgres
@@ -55,3 +55,13 @@ services:
       - DATABASE_HOST=localhost
     volumes:
       - ./.docker/postgres/data:/var/lib/postgresql/data
+  mssql2019:
+    image: mcr.microsoft.com/mssql/server:2019-latest
+    container_name: sqls_mssql2019
+    ports:
+      - "11433:1433"
+    environment:
+      SA_PASSWORD: Passw0rd
+      ACCEPT_EULA: Y
+    volumes:
+      - ./.docker/mssql/data:/var/opt/mssql/data

go.mod
@@ -3,14 +3,17 @@ module github.com/lighttiger2505/sqls
 go 1.16
 
 require (
+	github.com/denisenkom/go-mssqldb v0.11.0
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/google/go-cmp v0.5.6
+	github.com/jackc/fake v0.0.0-20150926172116-812a484cc733 // indirect
+	github.com/jackc/pgx v3.6.2+incompatible
 	github.com/jackc/pgx/v4 v4.12.0
 	github.com/k0kubun/colorstring v0.0.0-20150214042306-9440f1994b88 // indirect
 	github.com/k0kubun/pp v2.4.0+incompatible
 	github.com/mattn/go-sqlite3 v1.14.8
 	github.com/olekukonko/tablewriter v0.0.5
 	github.com/sourcegraph/jsonrpc2 v0.1.0
-	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
 	gopkg.in/yaml.v2 v2.4.0
 )

go.sum
@@ -45,6 +45,8 @@ github.com/creack/pty v1.1.7/go.mod h1:lj5s0c3V2DBrqTV7llrYr5NG6My20zk30Fl46Y7Do
 github.com/davecgh/go-spew v1.1.0/go.mod h1:J7Y8YcW2NihsgmVo/mv3lAwl/skON4iLHjSsI+c5H38=
 github.com/davecgh/go-spew v1.1.1 h1:vj9j/u1bqnvCEfJOwUhtlOARqs3+rkHYY13jYWTU97c=
 github.com/davecgh/go-spew v1.1.1/go.mod h1:J7Y8YcW2NihsgmVo/mv3lAwl/skON4iLHjSsI+c5H38=
+github.com/denisenkom/go-mssqldb v0.11.0 h1:9rHa233rhdOyrz2GcP9NM+gi2psgJZ4GWDpL/7ND8HI=
+github.com/denisenkom/go-mssqldb v0.11.0/go.mod h1:xbL0rPBG9cCiLr28tMa8zpbdarY27NDyej4t/EjAShU=
 github.com/dgrijalva/jwt-go v3.2.0+incompatible/go.mod h1:E3ru+11k8xSBh+hMPgOLZmtrrCbhqsmaPHjLKYnJCaQ=
 github.com/dustin/go-humanize v0.0.0-20171111073723-bb3d318650d4/go.mod h1:HtrtbFcZ19U5GC7JDqmcUSB87Iq5E25KnS6fMYU6eOk=
 github.com/eapache/go-resiliency v1.1.0/go.mod h1:kFI+JgMyC7bLPUVY133qvEBtVayf5mFgVsvEsIPBvNs=
@@ -76,6 +78,8 @@ github.com/gogo/googleapis v1.1.0/go.mod h1:gf4bu3Q80BeJ6H1S1vYPm8/ELATdvryBaNFG
 github.com/gogo/protobuf v1.1.1/go.mod h1:r8qH/GZQm5c6nD/R0oafs1akxWv10x8SbQlK7atdtwQ=
 github.com/gogo/protobuf v1.2.0/go.mod h1:r8qH/GZQm5c6nD/R0oafs1akxWv10x8SbQlK7atdtwQ=
 github.com/gogo/protobuf v1.2.1/go.mod h1:hp+jE20tsWTFYpLwKvXlhS1hjn+gTNwPg2I6zVXpSg4=
+github.com/golang-sql/civil v0.0.0-20190719163853-cb61b32ac6fe h1:lXe2qZdvpiX5WZkZR4hgp4KJVfY3nMkvmwbVkpv1rVY=
+github.com/golang-sql/civil v0.0.0-20190719163853-cb61b32ac6fe/go.mod h1:8vg3r2VgvsThLBIFL93Qb5yWzgyZWhEmBwUJWevAkK0=
 github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b/go.mod h1:SBH7ygxi8pfUlaOkMMuAQtPIUF8ecWP5IEl/CR7VP2Q=
 github.com/golang/groupcache v0.0.0-20160516000752-02826c3e7903/go.mod h1:cIg4eruTrX1D+g88fzRXU5OdNfaM+9IcxsU14FzY7Hc=
 github.com/golang/groupcache v0.0.0-20190702054246-869f871628b6/go.mod h1:cIg4eruTrX1D+g88fzRXU5OdNfaM+9IcxsU14FzY7Hc=
@@ -133,6 +137,8 @@ github.com/jackc/chunkreader v1.0.0/go.mod h1:RT6O25fNZIuasFJRyZ4R/Y2BbhasbmZXF9
 github.com/jackc/chunkreader/v2 v2.0.0/go.mod h1:odVSm741yZoC3dpHEUXIqA9tQRhFrgOHwnPIn9lDKlk=
 github.com/jackc/chunkreader/v2 v2.0.1 h1:i+RDz65UE+mmpjTfyz0MoVTnzeYxroil2G82ki7MGG8=
 github.com/jackc/chunkreader/v2 v2.0.1/go.mod h1:odVSm741yZoC3dpHEUXIqA9tQRhFrgOHwnPIn9lDKlk=
+github.com/jackc/fake v0.0.0-20150926172116-812a484cc733 h1:vr3AYkKovP8uR8AvSGGUK1IDqRa5lAAvEkZG1LKaCRc=
+github.com/jackc/fake v0.0.0-20150926172116-812a484cc733/go.mod h1:WrMFNQdiFJ80sQsxDoMokWK1W5TQtxBFNpzWTD84ibQ=
 github.com/jackc/pgconn v0.0.0-20190420214824-7e0022ef6ba3/go.mod h1:jkELnwuX+w9qN5YIfX0fl88Ehu4XC3keFuOJJk9pcnA=
 github.com/jackc/pgconn v0.0.0-20190824142844-760dd75542eb/go.mod h1:lLjNuW/+OfW9/pnVKPazfWOgNfH2aPem8YQ7ilXGvJE=
 github.com/jackc/pgconn v0.0.0-20190831204454-2fabfa3c18b7/go.mod h1:ZJKsE/KZfsUgOEh9hBm+xYTstcNHg7UPMVJqRfQxq4s=
@@ -172,6 +178,8 @@ github.com/jackc/pgtype v1.3.1-0.20200606141011-f6355165a91c/go.mod h1:cvk9Bgu/V
 github.com/jackc/pgtype v1.7.0/go.mod h1:ZnHF+rMePVqDKaOfJVI4Q8IVvAQMryDlDkZnKOI75BE=
 github.com/jackc/pgtype v1.8.0 h1:iFVCcVhYlw0PulYCVoguRGm0SE9guIcPcccnLzHj8bA=
 github.com/jackc/pgtype v1.8.0/go.mod h1:PqDKcEBtllAtk/2p6z6SHdXW5UB+MhE75tUol2OKexE=
+github.com/jackc/pgx v3.6.2+incompatible h1:2zP5OD7kiyR3xzRYMhOcXVvkDZsImVXfj+yIyTQf3/o=
+github.com/jackc/pgx v3.6.2+incompatible/go.mod h1:0ZGrqGqkRlliWnWB4zKnWtjbSWbGkVEFm4TeybAXq+I=
 github.com/jackc/pgx/v4 v4.0.0-20190420224344-cc3461e65d96/go.mod h1:mdxmSJJuR08CZQyj1PVQBHy9XOp5p8/SHH6a0psbY9Y=
 github.com/jackc/pgx/v4 v4.0.0-20190421002000-1b8f0016e912/go.mod h1:no/Y67Jkk/9WuGR0JG/JseM9irFbnEPbuWV2EELPNuM=
 github.com/jackc/pgx/v4 v4.0.0-pre1.0.20190824185557-6972a5742186/go.mod h1:X+GQnOEnf1dqHGpw7JmHqHc1NxDoalibchSk9/RWuDc=
@@ -365,6 +373,7 @@ go.uber.org/zap v1.13.0/go.mod h1:zwrFLgMcdUuIBviXEYEH1YKNaOBnKXsx2IPda5bBwHM=
 golang.org/x/crypto v0.0.0-20180904163835-0709b304e793/go.mod h1:6SG95UA2DQfeDnfUPMdvaQW0Q7yPrPDi9nlGo2tz2b4=
 golang.org/x/crypto v0.0.0-20181029021203-45a5f77698d3/go.mod h1:6SG95UA2DQfeDnfUPMdvaQW0Q7yPrPDi9nlGo2tz2b4=
 golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2/go.mod h1:djNgcEr1/C05ACkg1iLfiJU5Ep61QUkGW8qpdssI0+w=
+golang.org/x/crypto v0.0.0-20190325154230-a5d413f7728c/go.mod h1:djNgcEr1/C05ACkg1iLfiJU5Ep61QUkGW8qpdssI0+w=
 golang.org/x/crypto v0.0.0-20190411191339-88737f569e3a/go.mod h1:WFFai1msRO1wXaEeE5yQxYXgSfI8pQAWXbQop6sCtWE=
 golang.org/x/crypto v0.0.0-20190510104115-cbcb75029529/go.mod h1:yigFU9vqHzYiE8UmvKecakEJjdnWj3jj499lnFckfCI=
 golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4/go.mod h1:yigFU9vqHzYiE8UmvKecakEJjdnWj3jj499lnFckfCI=
@@ -376,8 +385,8 @@ golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9/go.mod h1:LzIPMQfyMNhhGPh
 golang.org/x/crypto v0.0.0-20201203163018-be400aefbc4c/go.mod h1:jdWPYTVW3xRLrWPugEBEK3UY2ZEsg3UU495nc5E+M+I=
 golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2/go.mod h1:T9bdIzuCu7OtxOm1hfPfRQxPLYneinmdGuTeoZ9dtd4=
 golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e/go.mod h1:GvvjBRRGRdwPK5ydBHafDWAxML/pGHZbMvKqRZ5+Abc=
-golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 h1:/UOmuWzQfxxo9UtlXMwuQU8CMgg1eZXqTRwkSQJWKOI=
-golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97/go.mod h1:GvvjBRRGRdwPK5ydBHafDWAxML/pGHZbMvKqRZ5+Abc=
+golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 h1:7I4JAnoQBe7ZtJcBaYHi5UtiO8tQHbUSXxL+pnGRANg=
+golang.org/x/crypto v0.0.0-20210921155107-089bfa567519/go.mod h1:GvvjBRRGRdwPK5ydBHafDWAxML/pGHZbMvKqRZ5+Abc=
 golang.org/x/exp v0.0.0-20190121172915-509febef88a4/go.mod h1:CJ0aWSM057203Lf6IL+f9T1iT9GByDxfZKAQTCR3kQA=
 golang.org/x/lint v0.0.0-20181026193005-c67002cb31c3/go.mod h1:UVdnD1Gm6xHRNCYTkRU2/jEulfH38KcIWyp/GAMgvoE=
 golang.org/x/lint v0.0.0-20190227174305-5b3e6a55c961/go.mod h1:wehouNa3lNwaWXcvxsM5YxQ5yQlVC4a0KAMCusXpPoU=

internal/database/config.go
@@ -72,6 +72,24 @@ func (c *DBConfig) Validate() error {
 		if c.DataSourceName == "" {
 			return errors.New("required: connections[].dataSourceName")
 		}
+	case dialect.DatabaseDriverMssql:
+		if c.DataSourceName == "" && c.Proto == "" {
+			return errors.New("required: connections[].dataSourceName or connections[].proto")
+		}
+		if c.DataSourceName == "" && c.Proto != "" {
+			if c.User == "" {
+				return errors.New("required: connections[].user")
+			}
+			switch c.Proto {
+			case ProtoTCP:
+				if c.Host == "" {
+					return errors.New("required: connections[].host")
+				}
+			case ProtoUDP, ProtoUnix:
+			default:
+				return errors.New("invalid: connections[].proto")
+			}
+		}
 	default:
 		return errors.New("invalid: connections[].driver")
 	}

commit b4af59040b445f6e905b9661280f356a857993b1

Fixed a bug in MSSQL connection
introduced in d2cda9c

internal/database/mssql.go
@@ -327,7 +327,7 @@ func genMssqlConfig(connCfg *DBConfig) (string, error) {
 		if port == 0 {
 			port = 1433
 		}
-		q.Set("host", host)
+		q.Set("server", host)
 		q.Set("port", strconv.Itoa(port))
 	case ProtoUDP, ProtoUnix:
 	default:

internal/database/mssql_test.go
@@ -30,7 +30,7 @@ func Test_genMssqlConfig(t *testing.T) {
 					"ApplicationIntent": "ReadOnly",
 				},
 			},
-			want:    "ApplicationIntent=ReadOnly;database=dvdrental;host=127.0.0.1;password=mysecretpassword1234;port=11433;user=sa",
+			want:    "ApplicationIntent=ReadOnly;database=dvdrental;password=mysecretpassword1234;port=11433;server=127.0.0.1;user=sa",
 			wantErr: false,
 		},
 	}